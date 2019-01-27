9aadb2257d7595e7d6136c03c03386358625b5de
Brandon A <7830127+bman511@users.noreply.github.com>
2019-01-27 12:40:04 -0500

    initial queries constructed

---
+++ sakilaQueries.sql
@@ -0,0 +1,63 @@
+
+SET SQL_SAFE_UPDATES = 0;
+
+#1a
+USE sakila;
+SELECT first_name, last_name
+FROM actor;
+
+#1b
+USE sakila;
+SELECT CONCAT(first_name," ",last_name) AS 'Actor Name'
+FROM actor;
+
+#2a
+USE sakila;
+SELECT first_name, last_name, actor_id
+FROM actor WHERE first_name = 'Joe';
+
+#2b
+USE sakila;
+SELECT first_name, last_name, actor_id
+FROM actor WHERE last_name LIKE '%GEN%';
+
+#2c
+USE sakila;
+SELECT first_name, last_name, actor_id
+FROM actor WHERE last_name LIKE '%LI%'
+ORDER BY last_name, first_name;
+
+#2d
+USE sakila;
+SELECT country_id, country
+FROM country
+WHERE country in ('China','Afghanistan','Bangladesh');
+
+#3a
+USE sakila;
+ALTER TABLE actor
+ADD COLUMN description BLOB;
+
+#3b
+USE sakila;
+ALTER TABLE actor
+DROP COLUMN description;
+
+#4a
+USE sakila;
+SELECT last_name, COUNT(last_name) AS 'Count'
+FROM actor
+GROUP BY last_name;
+
+#4b
+USE sakila;
+SELECT last_name, COUNT(last_name) AS 'Count' 
+FROM actor 
+GROUP BY last_name
+HAVING COUNT(last_name) > 1;
+
+#4c
+USE sakila;
+UPDATE actor
+SET first_name = 'HARPO'
+WHERE first_name = 'GROUCHO' AND last_name='WILLIAMS';
